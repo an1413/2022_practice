448cd943af7fb6811b1be1a5a6a0398893cf8cc5
Nabyeol An <an1413@hs.ac.kr>
2022-06-26 01:16:18 +0900

    operator

---
+++ Operators_prac1.java
@@ -0,0 +1,21 @@
+public class Operators_prac1 {
+    public static void main(String[] args){
+        int num1 = 10;
+        int num2 = 20;
+        int num3 = 30;
+
+        num1 = num2 = num3;
+        System.out.println(num1);
+        System.out.println(num2);
+
+        int num = 3;
+        int result = 0;
+        for(int i = 2; i <= 4; i++){
+            num += (3*i); 
+            System.out.println(num);
+        }
+
+        
+
+    }
+}
