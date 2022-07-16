315d46a4ed3b9cfc3c5bcf93f067cd173997221a
Nabyeol An <an1413@hs.ac.kr>
2022-07-16 14:00:05 +0900

    220716_commit

---
+++ Accumulator.java
@@ -0,0 +1,21 @@
+
+
+public class Accumulator {
+    
+    static int sum = 0;
+
+    static void add(int x){
+        sum += x;
+    }
+
+    static void showResult(){
+        System.out.println("sum = " + sum);
+    }
+
+    public static void main(String[] args){
+        for(int i = 0; i< 10; i++){
+            Accumulator.add(i);
+        }
+        Accumulator.showResult();
+    }
+}
